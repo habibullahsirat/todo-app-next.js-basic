956862bb92df4ff1a1dc8feedd5467e4fd71928e
Habibullah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-06 01:06:30 +0600

    Code updated.

---
+++ todo-app/app/api/route.ts
@@ -0,0 +1,31 @@
+import connectDB from "@lib/mongodb";
+import { Todo } from "@lib/models/Todo";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectDB();
+
+  const todos = await Todo.find();
+  const response = NextResponse.json(todos);
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+export async function POST(req: Request) {
+  try {
+    const { data } = await req.json();
+    await connectDB();
+    await Todo.create(data);
+    return NextResponse.json(
+      { message: "Todo created successfully" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    return NextResponse.json(
+      { message: "Error creating todo", error },
+      { status: 500 },
+    );
+  }
+}
--- todo-app/lib/mongodb.ts
+++ todo-app/lib/mongodb.ts
@@ -31,20 +31,19 @@
 
 // export default connectDB;
 
-
 import mongoose from "mongoose";
 
 export const connectToDB = async () => {
-    const dbUri = process.env.DB_URI as string;
-    if (!dbUri) {
-        throw new Error("DB_URI environment variable is not defined");
-    }
-    await mongoose
-        .connect(dbUri)
-        .then(() => {
-            console.log("MongoDB connected");
-        })
-        .catch((err) => {
-            console.error("MongoDB connection error:", err);
-        });
+  const dbUri = process.env.DB_URI as string;
+  if (!dbUri) {
+    throw new Error("DB_URI environment variable is not defined");
+  }
+  await mongoose
+    .connect(dbUri)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+    });
 };
