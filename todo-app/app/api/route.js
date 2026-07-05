91c0fac9712db53d14b22c5b7b8436d5bedc313e
Habibullah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-06 00:45:17 +0600

    GET method created.

--- todo-app/app/api/route.js
+++ todo-app/app/api/route.js
@@ -4,4 +4,10 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   await connectDB();
+
+  const todos = await Todo.find();
+  const response = NextResponse.json(todos);
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
 }
